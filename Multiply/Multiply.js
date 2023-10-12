54c4b5e5481402f8d4b3d7f96df23bc8ae943ccd
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-10-12 14:12:46 +0100

    Multiplay two numbers

---
+++ Multiply/Multiply.js
@@ -0,0 +1,10 @@
+function Multiply(a, b) {
+  return a * b;
+}
+
+console.log(Multiply(1, 2));
+console.log(Multiply(3, 5));
+console.log(Multiply(8, 1));
+console.log(Multiply(7, 5));
+console.log(Multiply(10, 23));
+
