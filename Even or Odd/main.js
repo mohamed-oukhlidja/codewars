867cf7e25283f1ac5aa0fb388bca5cfb4e5222f2
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-12-03 22:44:25 +0100

    Even or Odd peoblem solved

---
+++ Even or Odd/main.js
@@ -0,0 +1,14 @@
+function evenOrOdd(number) {
+    if (number % 2 ===0) {
+        return 'even'
+    }
+    return 'odd'
+}
+
+
+console.log(evenOrOdd(1));
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(3));
+console.log(evenOrOdd(4));
+console.log(evenOrOdd(5));
+console.log(evenOrOdd(6));
