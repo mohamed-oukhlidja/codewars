f2de9962e91f006efe631ba4e9e74e8dd59b0506
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-12-06 23:53:42 +0100

    add a case test

--- Sum of positive/main.js
+++ Sum of positive/main.js
@@ -11,5 +11,8 @@ function positiveSum(arr) {
 console.log(positiveSum([1, 2, 3, 4, 5]));
 console.log(positiveSum([-1, -2, -3, -4, -5]));
 console.log(positiveSum([1, -2, 3, -4, 5]));
+console.log(positiveSum([]));
+
+
 
 
